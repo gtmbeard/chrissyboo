f8791ca4de01ede5052f19a8ed60a974d27b2b92
gtmbeard <150093522+gtmbeard@users.noreply.github.com>
2023-11-06 19:26:14 -0500

    Update [...404].tsx

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -9,10 +9,10 @@ export default function NotFound() {
       <h1>Page Not Found</h1>
       <p>
         Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
+        <a href="https://fnaf1.netlify.app" target="_blank">
+          fnaf1.netlify.app
         </a>{" "}
-        to learn how to build SolidStart apps.
+        to play full fnaf 1.
       </p>
     </main>
   );
