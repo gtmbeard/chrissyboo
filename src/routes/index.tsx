164f5ab7774c3067501a15016450046f0bb0e1a2
gtmbeard <150093522+gtmbeard@users.noreply.github.com>
2023-11-06 19:43:04 -0500

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       <h1>Clicker by Christian</h1>
       <Counter />
       <p>
-        Visit{" "}
+        Click Here --->{" "}
         <a href="https://fnaf1.netlify.app" target="_blank">
           fnaf1.netlify.app
         </a>{" "}
